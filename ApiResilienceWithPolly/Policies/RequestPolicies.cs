2ed76777ffeddd31a262b0800bac9282e566dc55
François BOTTE <francois.botte@metsys.fr>
2022-11-07 21:36:42 +0100

    [FBO] : Update polly cricuit breaker

--- ApiResilienceWithPolly/Policies/RequestPolicies.cs
+++ ApiResilienceWithPolly/Policies/RequestPolicies.cs
@@ -1,18 +1,43 @@
 ﻿using Polly;
 using Polly.Retry;
+using Polly.CircuitBreaker;
+using Polly.Fallback;
 
 namespace ApiResilienceWithPolly.Policies
 {
     public class RequestPolicies
     {
-        
+
         public AsyncRetryPolicy<HttpResponseMessage> HttpRetryPolicy { get; }
+        public AsyncRetryPolicy<HttpResponseMessage> HttpWaitAndRetryPolicy { get; }
+        public AsyncCircuitBreakerPolicy<HttpResponseMessage> HttpCircuitBreakerPolicy { get; }
+        public AsyncCircuitBreakerPolicy<HttpResponseMessage> HttpCircuitBreakerWithCheckStatePolicy { get; }
         
         public RequestPolicies()
         {
             int retryCount = 5;
-            
-            HttpRetryPolicy = Policy.HandleResult<HttpResponseMessage>(result=> !result.IsSuccessStatusCode).RetryAsync(retryCount);
+
+            HttpRetryPolicy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode).RetryAsync(retryCount);
+
+            HttpWaitAndRetryPolicy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode)
+                                     .WaitAndRetryAsync(retryCount, retryAttempt => TimeSpan.FromSeconds(2));
+
+            HttpCircuitBreakerPolicy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode)
+                                     .CircuitBreakerAsync(1, TimeSpan.FromSeconds(20));
+
+            HttpCircuitBreakerWithCheckStatePolicy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode)
+                                     .CircuitBreakerAsync(1, TimeSpan.FromSeconds(20), CircuitToStateOpen, CircuitToStateClosed);
+        }
+
+        private void CircuitToStateOpen(DelegateResult<HttpResponseMessage> result, TimeSpan ts)
+        {
+            Console.WriteLine("Argh, Circuit en panne !");
+        }
+
+        private void CircuitToStateClosed()
+        {
+            Console.WriteLine("Ouf, Circuit revenu à la normale !");
         }
     }
 }
+
